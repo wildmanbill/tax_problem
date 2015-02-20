f9608cad3dfee29ebfe3b7c0bc195acf3ca586fc
louism2 <louiscmancini@gmail.com>
2015-02-19 17:50:10 -0800

    TaxableEntity attribute specs passing

--- app/models/taxable_entity.rb
+++ app/models/taxable_entity.rb
@@ -1,4 +1,20 @@
 class TaxableEntity < ActiveRecord::Base
   
+  COUNTRY_FORMAT = /\A[A-Za-z]{2}\Z/
+  ONLY_DIGITS = /[^\d]+/
+  
+  validates :name,     presence: true,
+                       length: {maximum: 50}
+                     
+  validates :country,  format: {with: COUNTRY_FORMAT}
+  
+  
+  
+  # def income= income
+  #   income.gsub!(ONLY_DIGITS, '')
+  #   write_attribute(:income, income)
+  # end
+                     
+  
   
 end
