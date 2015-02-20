5cc1c3301ce7341d340ddf3bbb3862296499bb48
louism2 <louiscmancini@gmail.com>
2015-02-19 21:44:04 -0800

    BusinessUser validations and setter specs passing

--- app/models/business_user.rb
+++ app/models/business_user.rb
@@ -1,5 +1,17 @@
 class BusinessUser < TaxableEntity
   
+  NOT_DIGITS = /[^\d]/
   
+  store_accessor :store, :business_id_number, :industry
+
+  validates :business_id_number,        presence: true,
+                                        numericality: true
+
+  validates :industry,                  presence: true
+
+  def business_id_number= id_number
+    self.store['business_id_number'] = id_number.gsub!(NOT_DIGITS, '')
+  end  
+
   
 end  
